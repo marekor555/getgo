0553d4abe2cfabe015e97885ffac2a0bd27d672b
MAREKOR555 <MAREKOR555@INTERIA.PL>
2024-05-11 16:37:59 +0200

    added better logging and file handling error are now displayed in more consistent way if file exists, wait 5 seconds for user to notice

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/pterm/pterm"
 )
@@ -19,17 +20,21 @@ func main() {
 	flag.Parse()
 	url := flag.Arg(0)
 
+	logger := pterm.DefaultLogger.WithLevel(pterm.LogLevelTrace)
 	spinner, _ := pterm.DefaultSpinner.Start("getting response...")
+
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err.Error())
+		spinner.Fail("")
+		logger.Fatal(err.Error())
 	}
 
 	sizeBytes, err := strconv.Atoi(resp.Header.Get("Content-Length"))
 	spinner.UpdateText(fmt.Sprintf("reading response of %vMB", sizeBytes/1000000))
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err.Error())
+		spinner.Fail("")
+		logger.Fatal(err.Error())
 	}
 
 	urlSplit := strings.Split(url, "/")
@@ -38,10 +43,21 @@ func main() {
 		filename = *saveas
 	}
 
+	workingDir, err := os.Getwd()
+	if err != nil {
+		spinner.Fail("")
+		logger.Fatal(err.Error())
+	}
+	if _, err := os.Stat(filepath.Join(workingDir, filename)); err == nil {
+		spinner.UpdateText("File exists, overwriting in 5s...")
+		time.Sleep(time.Second * 5)
+	}
+
 	if *saveFile {
 		err = os.WriteFile(filename, body, 0644)
 		if err != nil {
-			log.Fatal(err.Error())
+			spinner.Fail("")
+			logger.Fatal(err.Error())
 		}
 		spinner.Success("Succesfully saved file")
 	} else {
