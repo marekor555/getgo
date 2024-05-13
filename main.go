05c9685677429930e78512f960783317b84a66fe
MAREKOR555 <MAREKOR555@INTERIA.PL>
2024-05-13 18:21:49 +0200

    removed necessarity if https:// at start of the file documented project and repaired waiting
    this is alpha release, so I include LICENSE too

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ import (
 )
 
 func main() {
-	saveFile := flag.Bool("save", false, "")
-	saveas := flag.String("as", "", "")
+	saveFile := flag.Bool("save", false, "") // --save user wants to save the output
+	saveas := flag.String("as", "", "")      // --as user wants to change default name (for eg. with .html websites)
 	flag.Parse()
 	url := flag.Arg(0)
 
@@ -25,12 +25,15 @@ func main() {
 
 	resp, err := http.Get(url)
 	if err != nil {
-		spinner.Fail("")
-		logger.Fatal(err.Error())
+		resp, err = http.Get("https://" + url) // if normal http.Get fails, user probably forgot https://
+		if err != nil {
+			spinner.Fail("")
+			logger.Fatal(err.Error())
+		}
 	}
 
-	sizeBytes, err := strconv.Atoi(resp.Header.Get("Content-Length"))
-	spinner.UpdateText(fmt.Sprintf("reading response of %vMB", sizeBytes/1000000))
+	sizeBytes, err := strconv.Atoi(resp.Header.Get("Content-Length"))              // get filesize in bytes
+	spinner.UpdateText(fmt.Sprintf("reading response of %vMB", sizeBytes/1000000)) // update spinner text to show the file size
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		spinner.Fail("")
@@ -40,27 +43,27 @@ func main() {
 	urlSplit := strings.Split(url, "/")
 	filename := urlSplit[len(urlSplit)-1]
 	if *saveas != "" {
-		filename = *saveas
+		filename = *saveas // TODO: verify if it is actuall filename (different method?)
 	}
 
-	workingDir, err := os.Getwd()
+	workingDir, err := os.Getwd() // get working directory
 	if err != nil {
 		spinner.Fail("")
 		logger.Fatal(err.Error())
 	}
-	if _, err := os.Stat(filepath.Join(workingDir, filename)); err == nil {
-		spinner.UpdateText("File exists, overwriting in 5s...")
-		time.Sleep(time.Second * 5)
-	}
 
-	if *saveFile {
+	if *saveFile { //save the file if specified
+		if _, err := os.Stat(filepath.Join(workingDir, filename)); err == nil { // if file exists, user needs to be informed
+			spinner.UpdateText("File exists, overwriting in 5s...")
+			time.Sleep(time.Second * 5)
+		}
 		err = os.WriteFile(filename, body, 0644)
 		if err != nil {
 			spinner.Fail("")
 			logger.Fatal(err.Error())
 		}
 		spinner.Success("Succesfully saved file")
-	} else {
+	} else { // if nothing is specified to do with the file, dump the output
 		fmt.Println(string(body))
 		spinner.Success("Dumped data into term")
 	}
